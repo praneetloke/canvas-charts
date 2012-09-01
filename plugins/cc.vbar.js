38c3ac5dfb55b4d60a86ec2c7462c2b5524a8900
Dustin McCormick <djmccormick@gmail.com>
2012-09-01 01:38:48 -0400

    Resolved Issue #1

--- plugins/cc.vbar.js
+++ plugins/cc.vbar.js
@@ -1,13 +1,22 @@
 CanvasCharts.prototype.drawVBars = function(data, stepValue){
-    //start a little away from the y-axis
-    var x = this.startX + 10;
-    //create the gradient for the line
+    // Create the gradient for the line:
     this.createGradient();
-    //number of data points we need to plot
-    var length = data.length;
-    for(var i = 0 ; i < length ; i += 1){
-        //draw line from the current data point to the next one
-        this.drawLine(x, (this.containerHeight - 45), x, (this.containerHeight - data[i]), {"lineWidth": this.barHeight, "strokeStyle": this.barGradient});
+
+    // Start a little away from the y-axis:
+    var baseX = this.startX + 12;
+
+    // Draw a line for each data entry:
+    for(var i = 0 ; i < data.length ; i += 1){
+        // From the bottom up to the current data point:
+        this.drawLine(
+            baseX, this.containerHeight - 45,
+            baseX, this.containerHeight - 45 - data[i],
+            {
+                "lineWidth": this.barHeight,
+                "strokeStyle": this.barGradient
+            }
+        );
+
         x += stepValue;
     }
 };
