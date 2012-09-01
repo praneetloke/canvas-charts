a180f66a1c17740b0cefb22b8decf6729fcfcb2a
Dustin McCormick <djmccormick@gmail.com>
2012-09-01 01:16:25 -0400

    Normalized whitespace and removed any trailing whitespace on each line.

--- plugins/cc.vbar.js
+++ plugins/cc.vbar.js
@@ -1,5 +1,5 @@
 CanvasCharts.prototype.drawVBars = function(data, stepValue){
-	//start a little away from the y-axis
+    //start a little away from the y-axis
     var x = this.startX + 10;
     //create the gradient for the line
     this.createGradient();
@@ -7,7 +7,7 @@ CanvasCharts.prototype.drawVBars = function(data, stepValue){
     var length = data.length;
     for(var i = 0 ; i < length ; i += 1){
         //draw line from the current data point to the next one
-		this.drawLine(x, (this.containerHeight - 45), x, (this.containerHeight - data[i]), {"lineWidth": this.barHeight, "strokeStyle": this.barGradient});
-		x += stepValue;
+        this.drawLine(x, (this.containerHeight - 45), x, (this.containerHeight - data[i]), {"lineWidth": this.barHeight, "strokeStyle": this.barGradient});
+        x += stepValue;
     }
 };
